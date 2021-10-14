de1727bb1afea2975c8411fc6408abb7951dc571
darkLord <amit.saha@wsegames.com>
2021-10-15 00:57:38 +0530

    husky with prettier

--- app/modules/Quote/Routes.js
+++ app/modules/Quote/Routes.js
@@ -1,13 +1,12 @@
 module.exports = (app, express) => {
+  const router = express.Router();
 
-    const router = express.Router();
+  router.get("/quote", (req, res) => {
+    res.send("Quote api working");
+  });
+  router.get("/fot", (req, res) => {
+    res.send("fot api working");
+  });
 
-    router.get('/quote', (req, res) => {
-        res.send('Quote api working')
-    })
-    router.get('/fot', (req, res) => {
-        res.send('fot api working')
-    })
-
-    app.use(process.env.baseApiUrl, router);
-}
+  app.use(process.env.baseApiUrl, router);
+};
--- config/db.js
+++ config/db.js
@@ -1,17 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-        console.log(`MongoDB connected: ${conn.connection.host}`)
-    } catch (err) {
-        console.log(err)
-        process.exit(1)
-    }
-}
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
