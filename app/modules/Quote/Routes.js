217606068b1ec2dec719a5ad12c9e7c2b0a6491e
Amit Saha <darklord@Amits-Mac-mini.local>
2021-10-14 22:55:32 +0530

    initial commit

---
+++ app/modules/Quote/Routes.js
@@ -0,0 +1,13 @@
+module.exports = (app, express) => {
+
+    const router = express.Router();
+
+    router.get('/quote', (req, res) => {
+        res.send('Quote api working')
+    })
+    router.get('/fot', (req, res) => {
+        res.send('fot api working')
+    })
+
+    app.use(process.env.baseApiUrl, router);
+}
